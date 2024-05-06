680809fc5a142f6623732676ad5754b3946dd453
Adeonir Kohl <adeonir@gmail.com>
2024-05-06 17:27:27 -0300

    chore: fix lint errors

--- src/classes/GeoJSON.ts
+++ src/classes/GeoJSON.ts
@@ -1,38 +1,38 @@
 interface IPoint {
-  type: "Point" | "Polygon" | "Line" | "MultiPolygon" | "Multiline";
+  type: 'Point' | 'Polygon' | 'Line' | 'MultiPolygon' | 'Multiline';
   coordinates: number[];
 }
 
 class Point implements IPoint {
-  public type: "Point" | "Polygon" | "Line" | "MultiPolygon" | "Multiline";
+  public type: 'Point' | 'Polygon' | 'Line' | 'MultiPolygon' | 'Multiline';
   public coordinates: number[];
 
   constructor(coordinates: number[]) {
-    this.type = "Point";
+    this.type = 'Point';
     this.coordinates = coordinates;
   }
 }
 
 interface IFeature {
-  type: "Feature";
+  type: 'Feature';
   geometry: IPoint;
   properties: { [key: string]: unknown };
 }
 
 class Feature implements IFeature {
-  public type: "Feature";
+  public type: 'Feature';
   public geometry: IPoint;
   public properties: { [key: string]: unknown };
 
   constructor(coordinates: number[], properties: { [key: string]: unknown }) {
-    this.type = "Feature";
+    this.type = 'Feature';
     this.geometry = new Point(coordinates);
     this.properties = properties;
   }
 }
 
 interface IGeoJSON {
-  type: "FeatureCollection";
+  type: 'FeatureCollection';
   features: IFeature[];
 }
 
@@ -46,11 +46,11 @@ interface IGeoJSONConstructor {
 }
 
 export class GeoJSON implements IGeoJSON {
-  public type: "FeatureCollection";
+  public type: 'FeatureCollection';
   public features: IFeature[];
 
   constructor({ coordinatesWithData }: IGeoJSONConstructor) {
-    this.type = "FeatureCollection";
+    this.type = 'FeatureCollection';
     this.features = [];
     this.concatCoordinates(coordinatesWithData);
   }
--- src/components/MarkerBottomSheetData/index.tsx
+++ src/components/MarkerBottomSheetData/index.tsx
@@ -1,4 +1,4 @@
-import { FormattedCSVDataProperties } from "../../services/getCSVData";
+import type { FormattedCSVDataProperties } from '../../services/getCSVData';
 
 type MarkerBottomSheetDataProps = {
   markerToDisplay: FormattedCSVDataProperties;
@@ -8,7 +8,7 @@ export const MarkerBottomSheetData = ({
   markerToDisplay,
 }: MarkerBottomSheetDataProps) => {
   return (
-    <div className="py-16 px-4 bg-white shadow rounded">
+    <div className="rounded bg-white px-4 py-16 shadow">
       <h2 className="text-[1rem] font-bold">Local: {markerToDisplay.LOCAL}</h2>
 
       <p className="text-[0.8rem]">{markerToDisplay.ENDERECO}</p>
@@ -23,11 +23,15 @@ export const MarkerBottomSheetData = ({
 
       <p className="text-[0.8rem]">Observações: {markerToDisplay.OBS}</p>
 
-      {markerToDisplay["GRUPO WHATS"] ? (
+      {markerToDisplay['GRUPO WHATS'] ? (
         <p className="text-[0.8rem]">
-          Whatsapp:{" "}
-          <a href={markerToDisplay["GRUPO WHATS"]} target="_blank">
-            {markerToDisplay["GRUPO WHATS"]}
+          Whatsapp:{' '}
+          <a
+            href={markerToDisplay['GRUPO WHATS']}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {markerToDisplay['GRUPO WHATS']}
           </a>
         </p>
       ) : null}
