3f19d04b6da7da37ecab4174a76ede5a0328dd5e
Cicera Ribeiro <maccicera@gmail.com>
2024-05-06 21:08:15 -0300

    feat: passando max-h de 200px e scroll

--- src/components/MapSearch/index.tsx
+++ src/components/MapSearch/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import type { FormattedCSVData } from "../../services/getCSVData";
 import { Input } from "../Input";
 import { Search } from "../../icons";
+import "../../styles/globals.css";
 
 type MapSearchProps = {
   coordinatesWithData: FormattedCSVData[] | undefined;
@@ -53,28 +54,31 @@ export const MapSearch = ({
         endAdornment={<Search className="absolute right-3 top-1/2 transform -translate-y-1/2 scale-75" />}
       />
       {filteredData.length > 0 ? (
-        <ul className="absolute w-full bg-white rounded-t-none rounded-b-[28px] border-t-[1px] border-gray-border shadow-lg gap-4 ">
-          {filteredData.map((data, index) => {
-            const { LOCAL = "", ENDERECO = "" } = data.properties;
-            const isLastItem = index === filteredData.length - 1;
-            return (
-              <li
-                key={index}
-                className={`pt-8 pr-24 pb-8 pl-16 cursor-pointer hover:bg-gray-100 flex flex-col ${isLastItem ? "rounded-b-[28px]" : ""
-                  }`}
-                onClick={() =>
-                  flyToCoordinate([data.coordinates[0], data.coordinates[1]])
-                }
-              >
-                <p className="text-gray-800 font-inter font-normal text-sm leading-6">{LOCAL}</p>
-                <p className="text-gray-500 font-poppins text-xs">{ENDERECO}</p>
-              </li>
-            );
-          })}
-        </ul>
+        <div >
+          <ul className="relative scrollbar-hidden overflow-y-auto max-h-[200px] w-full bg-white rounded-t-none rounded-b-[28px] border-t-[1px] border-gray-border shadow-lg gap-4">
+            {filteredData.map((data, index) => {
+              const { LOCAL = "", ENDERECO = "" } = data.properties;
+              const isLastItem = index === filteredData.length - 1;
+              return (
+                <li
+                  key={index}
+                  className={`pt-8 pr-24 pb-8 pl-16 cursor-pointer hover:bg-gray-100 flex flex-col ${isLastItem ? "rounded-b-[28px]" : ""
+                    }`}
+                  onClick={() =>
+                    flyToCoordinate([data.coordinates[0], data.coordinates[1]])
+                  }
+                >
+                  <p className="text-gray-800 font-inter font-normal text-sm leading-6">{LOCAL}</p>
+                  <p className="text-gray-500 font-poppins text-xs">{ENDERECO}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       ) : (
         null
       )}
+
     </div>
   );
 };
