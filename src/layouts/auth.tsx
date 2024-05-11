51b6e7f484f41c96712b823eb19c904b0c136611
Adeonir Kohl <adeonir@gmail.com>
2024-05-11 09:13:51 -0300

    feat: add sign in form

--- src/layouts/auth.tsx
+++ src/layouts/auth.tsx
@@ -7,14 +7,12 @@ export const AuthLayout = () => {
     <div className="min-h-screen w-full lg:grid lg:grid-cols-2">
       <section className="hidden h-full items-center justify-center border-r-2 border-foreground/5 bg-muted p-10 lg:flex">
         <img
-          className="size-48"
+          className="h-auto w-full max-w-56"
           src={logo}
           alt="Mapa Solidário"
-          width="345"
-          height="329"
         />
       </section>
-      <section className="flex items-center justify-center p-10">
+      <section className="flex h-screen items-center justify-center p-4 sm:p-10">
         <Outlet />
       </section>
     </div>
