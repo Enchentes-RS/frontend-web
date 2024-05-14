beb95e3b6846dd373d0ffecf79f3e5dc21ae6438
Adeonir Kohl <adeonir@gmail.com>
2024-05-13 22:21:18 -0300

    fix: update navitem export

--- src/components/navitem.tsx
+++ src/components/navitem.tsx
@@ -1,8 +1,10 @@
 import type { LinkProps } from 'react-router-dom'
 import { Link, useLocation } from 'react-router-dom'
 
-export const NavItem = (props: LinkProps) => {
+const NavItem = (props: LinkProps) => {
   const { pathname } = useLocation()
 
   return <Link data-active={pathname === props.to} {...props} />
 }
+
+export { NavItem }
