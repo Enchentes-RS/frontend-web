340dbd2263bfec63f91370eea813d4444b524738
souzzagab <gabriel.souza@lacreisaude.com.br>
2024-05-14 00:48:57 -0300

    feature: popup with location

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,22 +1,18 @@
-import { useLocalsQuery } from "@/api/locals";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Icon } from "leaflet";
 import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import iconUrl from "leaflet/dist/images/marker-icon.png";
 import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import centrosVoluntariosRS from "@/../centros_voluntarios_rs.json";
 
 const customIcon = new Icon({
-  iconRetinaUrl: iconRetinaUrl,
-  iconUrl: iconUrl,
-  shadowUrl: shadowUrl,
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
 });
 
 export const MapPage = () => {
-  const { data: locals } = useLocalsQuery();
-
-  console.log(locals);
-
   return (
     <div>
       <MapContainer center={[-30.033, -51.23]} zoom={13}>
@@ -24,11 +20,18 @@ export const MapPage = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[-30.033, -51.23]} icon={customIcon}>
-          <Popup>
-           DOAÇÕES <br /> AQUI VAI O CARD DE DOAÇÕES
-          </Popup>
-        </Marker>
+        {centrosVoluntariosRS.map((centro, index) => (
+          <Marker
+            key={index}
+            position={[
+              Number(centro.Latitude) || 0,
+              Number(centro.Longitude) || 0,
+            ]}
+            icon={customIcon}
+          >
+            <Popup/>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
