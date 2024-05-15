488162a2f2d44fb7901b23ba166cac0cea86edac
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-14 23:37:31 -0300

    refactor: Remove unused donation and volunteer schemas and types

--- src/types/donation.ts
+++
@@ -1,5 +0,0 @@
-import type { z } from 'zod'
-
-import type { donationStatusSchema } from '@/schemas/donation'
-
-export type Donation = z.infer<typeof donationStatusSchema>
--- src/types/volunteer.ts
+++
@@ -1,5 +0,0 @@
-import type { z } from 'zod'
-
-import type { volunteerStatusSchema } from '@/schemas/volunteer'
-
-export type Donation = z.infer<typeof volunteerStatusSchema>
