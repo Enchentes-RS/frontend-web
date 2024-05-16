028d4fabe9fdc29b6f57c880ae9c2a77481e5abc
João Paulo Martins <jpamdev@gmail.com>
2024-05-16 10:50:16 -0300

    Update src/components/info/donation-instruction.tsx
    Co-authored-by: Adeonir Kohl <adeonir@gmail.com>

--- src/components/info/donation-instruction.tsx
+++ src/components/info/donation-instruction.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import { cn } from '@/lib/utils'
 
-type DonationInstructionProps = {
+interface DonationInstructionProps = {
   keyType: string
   keyValue: string
 }
