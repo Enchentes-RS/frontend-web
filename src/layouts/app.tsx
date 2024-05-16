c9bc87dd6c80ba2296927d4ffd3680984d59c3ad
Wilson Faustino <wilson.rfaustino@gmail.com>
2024-05-16 00:18:51 -0300

    refactor: Update app layout to include Toaster component

--- src/layouts/app.tsx
+++ src/layouts/app.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom'
 
 import { Menu } from '@/components/menu'
+import { Toaster } from '@/components/ui/toaster'
 
 export const AppLayout = () => {
   return (
-    <main className="grid min-h-dvh grid-cols-1 grid-rows-[1fr_96px] sm:grid-cols-[112px_1fr] sm:grid-rows-1">
-      <Menu className="order-2 sm:order-1" />
-      <section className="order-1 sm:order-2">
-        <Outlet />
-      </section>
-    </main>
+    <>
+      <main className="grid min-h-dvh grid-cols-1 grid-rows-[1fr_96px] sm:grid-cols-[112px_1fr] sm:grid-rows-1">
+        <Menu className="order-2 sm:order-1" />
+        <section className="order-1 sm:order-2">
+          <Outlet />
+        </section>
+      </main>
+      <Toaster />
+    </>
   )
 }
