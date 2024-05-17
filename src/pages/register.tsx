52f78cc4519246ea0f31d40da9e6f2436667d086
Adeonir Kohl <adeonir@gmail.com>
2024-05-17 18:10:48 -0300

    fix: update margins on pages

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,7 +6,7 @@ export const RegisterPage = () => {
   return (
     <>
       <Helmet title="Cadastrar Local" />
-      <div className="mx-auto flex w-full flex-col gap-8 px-4 pb-5 pt-12 sm:max-w-xl md:px-0">
+      <div className="mx-auto flex w-full flex-col gap-8 px-4 py-6 sm:max-w-2xl">
         <header className="flex flex-col gap-2">
           <h1 className="text-3xl font-bold">Cadastrar Local</h1>
           <p className="text-base font-normal text-zinc-600">
